2198c99bd482cff3aa9d6f33d4ffbb2f6d62b3ed
Oleg Osipov <greenweb73@gmail.com>
2022-11-02 12:58:42 +0200

    fix background size and scroll screen for mobile

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,11 +1,7 @@
 <template>
 
-    <div class=" min-h-screen sm:items-center sm:pt-0 overflow-hidden bg-no-repeat bg-cover"
-         style="
-    background-position: 50%;
-    background-image: url('/images/orion-constellation.jpg');
-    height: 350px;
-  ">
+    <div class=" min-h-screen sm:items-center sm:pt-0 bg-re bg-repeat-y "
+         style="background-image: url('/images/orion-constellation.jpg');">
         <div class="container-fluid md:container mx-auto">
             <slot />
         </div>
